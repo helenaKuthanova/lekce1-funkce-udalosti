8d834783db1cb68feb22a09427fa8c9653dafa0a
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-10 08:32:51 +0100

    domaci ukol

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
 
-/**
- * Sečte dvě čísla na vstupu a vrátí výsledek
- *
- * @param {int} a
- * @param {int} b
- */
+// Sečte dvě čísla na vstupu a vrátí výsledek
 function secti(a, b) {
   let c = a + b;
   return c;
 }
 
+// Zobrazí výsledek (po stisknutí -- to zadám v HTML)
+function zobrazVysledek(a, b) {
+    let vysledek = document.querySelector('#vysledek');
+    vysledek.innerHTML = secti(a, b);
+}
+
 /**
  * Upozorní uživatele při spuštění.
  */
-function upozorni() {
+function upozorni(element) {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log('Gratulace, konečně jsi spustila tu funkci!');
+  zamenText(element)
+}
+
+/* zamění text ve vybraném elementu */
+function zamenText(selektor) {
+    let prvek = document.querySelector(selektor)
+    prvek.textContent = 'Konečně!'
+}
+
+/* změní barvu zadaného prvku */
+function zmenBarvuNaZelenou(selektor) {
+    let prvek = document.querySelector(selektor);
+    prvek.style.backgroundColor = "green";
+}
+
+function zmenBarvuNaZlutou(selektor) {
+    let prvek = document.querySelector(selektor);
+    prvek.style.backgroundColor = "yellow";
+}
+
+function spustiPokusnyRadek(id) {
+    let pokusnyRadek = document.querySelector(id)
+    pokusnyRadek.innerHTML = '<em>Zkouším pokus na pokusném řádku.<\em>'
 }
